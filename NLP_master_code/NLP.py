862208e3c85321bbc8f78186ce95782c32244bc2
suhaque <suhaque@cpp.edu>
2017-02-23 19:39:51 -0800

    Update NLP.py

--- NLP_master_code/NLP.py
+++ NLP_master_code/NLP.py
@@ -1,158 +1,66 @@
-import nltk
-import random
-#from nltk.corpus import movie_reviews
-###EE
-from nltk.classify.scikitlearn import SklearnClassifier
-import pickle
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.svm import SVC, LinearSVC, NuSVC
-from nltk.classify import ClassifierI
-from statistics import mode
-from nltk.tokenize import word_tokenize
-import sys  
+from vaderSentiment import vaderSentiment
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import time
+import speech_recognition as sr
+import pocketsphinx 
 
-reload(sys)  
-sys.setdefaultencoding('Cp1252')
-
-
-class VoteClassifier(ClassifierI):
-    def __init__(self, *classifiers):
-        self._classifiers = classifiers
-
-    def classify(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-        return mode(votes)
-
-    def confidence(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-
-        choice_votes = votes.count(mode(votes))
-        conf = choice_votes / len(votes)
-        return conf
+speak = True
+while(speak):
+    # obtain audio from the microphone
+    r = sr.Recognizer()
     
-short_pos = open("/Users/hafizahmed/Documents/Courses/SeniorProject/positive.txt","r").read()
-short_neg = open("/Users/hafizahmed/Documents/Courses/SeniorProject/negative.txt","r").read()
 
-# move this up here
-all_words = []
-documents = []
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("Say something!")
+        audio = r.listen(source)
+        
+        try:
+            print("Sphinx thinks you said: " + r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+        try:
+            print("Google Speech Recognition thinks you said: " + r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    cmd = input('Did we detect what you said correctly? y/n ')
+    if cmd.upper() == 'Y':
+        speak = False
+    elif cmd.upper() == 'N':
+        speak = True
+    else: print('invalid command')
 
+command = True    
+while(command):
+    ins = input("Do you want to analyze? Y/n ")
 
-#  j is adject, r is adverb, and v is verb
-#allowed_word_types = ["J","R","V"]
-allowed_word_types = ["J"]
-
-for p in short_pos.split('\n'):
-    documents.append( (p, "pos") )
-    words = word_tokenize(p)
-    pos = nltk.pos_tag(words)
-    for w in pos:
-        if w[1][0] in allowed_word_types:
-            all_words.append(w[0].lower())
-
+    if ins.upper() == 'Y':
+        file = open('test.txt','w')
+        gorsp = input("From google or sphinx? g/s ")
+        if gorsp.upper() == 'G':
+            file.write(r.recognize_google(audio))
+            file.close()
+            command = False
+        elif gorsp.upper() == 'S':
+            file.write(r.recognize_sphinx(audio))
+            file.close()
+            command = False
+        else: 
+            print ('invalid command')
+            command = True
+    elif ins.upper() == 'N':
+        command = False
+    else:
+        print('Invalid command')
+        command = True
     
-for p in short_neg.split('\n'):
-    documents.append( (p, "neg") )
-    words = word_tokenize(p)
-    pos = nltk.pos_tag(words)
-    for w in pos:
-        if w[1][0] in allowed_word_types:
-            all_words.append(w[0].lower())
-
-
-
-save_documents = open("documents.pickle","wb")
-pickle.dump(documents, save_documents)
-save_documents.close()
-
-
-all_words = nltk.FreqDist(all_words)
-
-
-word_features = list(all_words.keys())[:5000]
-
-
-save_word_features = open("word_features5k.pickle","wb")
-pickle.dump(word_features, save_word_features)
-save_word_features.close()
-
-
-def find_features(document):
-    words = word_tokenize(document)
-    features = {}
-    for w in word_features:
-        features[w] = (w in words)
-
-    return features
-
-featuresets = [(find_features(rev), category) for (rev, category) in documents]
-
-random.shuffle(featuresets)
-print(len(featuresets))
-
-testing_set = featuresets[10000:]
-training_set = featuresets[:10000]
-
-
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
-classifier.show_most_informative_features(15)
-
-###############
-save_classifier = open("originalnaivebayes5k.pickle","wb")
-pickle.dump(classifier, save_classifier)
-save_classifier.close()
-
-MNB_classifier = SklearnClassifier(MultinomialNB())
-MNB_classifier.train(training_set)
-print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
-
-save_classifier = open("MNB_classifier5k.pickle","wb")
-pickle.dump(MNB_classifier, save_classifier)
-save_classifier.close()
-
-BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
-BernoulliNB_classifier.train(training_set)
-print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/BernoulliNB_classifier5k.pickle","wb")
-pickle.dump(BernoulliNB_classifier, save_classifier)
-save_classifier.close()
-
-LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
-LogisticRegression_classifier.train(training_set)
-print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/LogisticRegression_classifier5k.pickle","wb")
-pickle.dump(LogisticRegression_classifier, save_classifier)
-save_classifier.close()
-
-
-LinearSVC_classifier = SklearnClassifier(LinearSVC())
-LinearSVC_classifier.train(training_set)
-print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
-
-save_classifier = open("pickled_algos/LinearSVC_classifier5k.pickle","wb")
-pickle.dump(LinearSVC_classifier, save_classifier)
-save_classifier.close()
-
-
-##NuSVC_classifier = SklearnClassifier(NuSVC())
-##NuSVC_classifier.train(training_set)
-##print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
-
-
-SGDC_classifier = SklearnClassifier(SGDClassifier())
-SGDC_classifier.train(training_set)
-print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
-
-save_classifier = open("pickled_algos/SGDC_classifier5k.pickle","wb")
-pickle.dump(SGDC_classifier, save_classifier)
-save_classifier.close()
+file = open("test.txt","r")
+sentences = [file.read()]
+analyzer = SentimentIntensityAnalyzer()
+for sentence in sentences:
+    vs = analyzer.polarity_scores(sentence)
+    print(vs)
